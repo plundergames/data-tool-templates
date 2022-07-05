446f03a4339415d03e9037af850ffa804577d6c5
plundergames <plundergamesdev@gmail.com>
2022-07-05 15:52:45 +0200

    Add template repository

---
+++ build.js
@@ -0,0 +1,54 @@
+import {readdir, readFile, mkdir, writeFile} from "fs/promises";
+import {join} from "path";
+
+const rootDir = "./templates";
+const buildDir = "./build";
+const allTemplatesFile = join(buildDir, "all.json");
+
+const categoryMapping = {
+  "health": "Health",
+  "media": "Media"
+}
+
+const getDirectories = async source =>
+  (await readdir(source, {withFileTypes: true}))
+    .filter(dirent => dirent.isDirectory())
+    .map(({name}) => name);
+
+const getFiles = async source =>
+  (await readdir(source, {withFileTypes: true}))
+    .filter(dirent => dirent.isFile())
+    .map(({name}) => name);
+
+const categories = await getDirectories(rootDir);
+
+async function getPreviews(category) {
+  const categoryPath = join(rootDir, category);
+  const templateNames = await getFiles(categoryPath);
+  const buildCategoryPath = join(buildDir, category);
+  await mkdir(buildCategoryPath, { recursive: true });
+
+  const templateProms = templateNames.map(async (templateName) => {
+    const templatePath = join(categoryPath, templateName);
+    const buildTemplatePath = join(buildCategoryPath, templateName);
+
+    const templateFile = await readFile(templatePath, "utf8");
+    const templateJson = JSON.parse(templateFile);
+    await writeFile(buildTemplatePath, JSON.stringify(templateJson));
+
+    const url = `${category}/${templateName}`;
+    return {
+      name: templateJson.name,
+      url,
+      steps: templateJson.steps || null
+    };
+  });
+
+  return Promise.all(templateProms);
+}
+
+const result = await Promise.all(
+  categories.map(category => getPreviews(category))
+)
+
+await writeFile(allTemplatesFile, JSON.stringify(result));
